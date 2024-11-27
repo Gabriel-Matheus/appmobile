bf5804ffad219e6c7f405a460dab3e5996442dbc
gabriel-matheus <gabrielmatheusgmcf@gmail.com>
2024-11-27 00:38:38 -0300

    App V1.0

--- app/src/main/java/App/appmobile/MainActivity.java
+++ app/src/main/java/App/appmobile/MainActivity.java
@@ -1,12 +1,17 @@
 package App.appmobile;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,18 +20,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
-    //acessando db
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+FirebaseFirestore db = FirebaseFirestore.getInstance();
+int id = 0;
 
-    //instanciando botões e edittexts
-    private Button bt_previous, bt_foward;
-    private EditText text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +41,15 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //função para instanciar
-        StartComponents();
+        //Acessar banco de dados
+        DocumentReference docRef = db.collection("textos").document("frases");
 
-        //listener do botão
-        bt_foward.setOnClickListener(new View.OnClickListener()
-        {
-            DocumentReference reference = db.collection("Frases").document("frases");
-            @Override
-            public void onClick(View v)
-            {
-                //acessando dados dentro do firestore
-                reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        DocumentSnapshot documentSnapshot = task.getResult();
-                        text.setText((String) documentSnapshot.getData().get("olhe pra cima"));
-                    }
-                });
-            }
-        });
+        //Instanciar Botões e textos
+        TextView view = findViewById(R.id.text);
+        Button bt_prev = findViewById(R.id.buttonprevious);
+        Button bt_next = findViewById(R.id.buttonnext);
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -62,16 +58,59 @@ public class MainActivity extends AppCompatActivity {
             return insets;
 
 
-
-
         });
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                //Coletando Frases no DB
+                DocumentSnapshot document = task.getResult();
+
+
+        //Avançar Frases
+        bt_next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Mudando Frases no APP
+                id++;
+                String idString = Integer.toString(id);
+                view.setText(document.getString(idString));
+
+                }
+
+
+                });
+
+        //Retornar Frases
+        bt_prev.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Mudando Frases no APP
+                if (id>1){
+                    id--;
+                    String idString = Integer.toString(id);
+                    view.setText(document.getString(idString));
+
+                }
+
+
+                }
+
+
+                });
+
+                }
+                });
+
+
+
     }
 
-    private void StartComponents(){
-        bt_previous = findViewById(R.id.buttonprevious);
-        bt_foward = findViewById(R.id.buttonnext);
-        text = findViewById(R.id.text);
-    }
 
 
 }
